124e7a1381875ae3886baf21ba6e9cc1ce8c6b31
Shruti Asthana <54969464+shrutiasthana2097@users.noreply.github.com>
2023-06-27 08:48:37 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,6 @@
+//https://leetcode.com/problems/two-sum/
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.*/
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
