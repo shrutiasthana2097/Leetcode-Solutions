661444d01a182c90a1061e1ae8a56e22c3b35505
Shruti <54969464+shrutiasthana2097@users.noreply.github.com>
2023-07-07 14:13:22 +0530

    Create BestTimetoBuySellStocksII.java

---
+++ BestTimetoBuySellStocksII.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+//Find and return the maximum profit you can achieve.
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
